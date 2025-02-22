9212194915f9d03a15e963c950cc1d0a1234f208
Dharmesh Patel <dharmesh@infineit.in>
2025-02-22 19:30:10 +0530

    feat: add new function 14

--- packages/hooks/src/useInput.ts
+++ packages/hooks/src/useInput.ts
@@ -10,6 +10,6 @@ const useInput = (initialValue = '') => {
   return { value, onChange };
 };
 
-// change 1.1.0 gf vlss haah ffdsdf
+// change 1.1.0 gf vlss haah ffdsdf sd
 
 export default useInput;
